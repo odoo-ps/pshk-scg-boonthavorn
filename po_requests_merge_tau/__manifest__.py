8f5e45c8e6b391117ea62702db0a9dc42c7afc5c
tau-odoo <tau@odoo.com>
2021-01-14 16:31:03 +0800

    [ADD] po_requests_merge_tau

---
+++ po_requests_merge_tau/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+'name': "po_requests_merge_tau",
+'version': '14.0.1.0',
+'author': 'OdooHK-TAU',
+'description': 'po request merge action',
+'depends': ['purchase'],
+'data': [
+    'views/merge_po_requests.xml',
+    ],
+}
