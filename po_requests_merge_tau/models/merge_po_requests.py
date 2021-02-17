af4a264e2fb38765d9155a4edebd641537e8240a
tau-odoo <tau@odoo.com>
2021-02-17 18:52:31 +0800

    [IMP] po_requests_merge_tau: enhance the po merge

--- po_requests_merge_tau/models/merge_po_requests.py
+++ po_requests_merge_tau/models/merge_po_requests.py
@@ -4,7 +4,6 @@ from odoo.exceptions import UserError
 class PurchaseOrder(models.Model):
     _inherit = "purchase.order"
 
-
     def action_merge_po_requests(self):
         """
             merge same vendor po requests
@@ -22,12 +21,37 @@ class PurchaseOrder(models.Model):
         if not all(vendor == purchase_order.partner_id for purchase_order in self):
             raise UserError(_('Vendor should be the same.'))
 
+        # check all are from same picking_type_id
+        picking_type_id = self[0].picking_type_id
+        if not all(picking_type_id == purchase_order.picking_type_id for purchase_order in self):
+            raise UserError(_('Operation type should be the same.'))
+
         # start merging
         merged_po = self.env['purchase.order'].create({
             'partner_id': vendor.id
         })
 
-        self.mapped('order_line').write({'order_id': merged_po.id})
+        # # check if order line has a sale_order_id in selected PO
+        # if self.mapped('order_line.sale_line_id'):
+
+        # self[0].mapped('order_line').write({'order_id': merged_po.id}) # merge the first purchase order
+
+        for purchase_order in self: # loop remaining purchase order
+            for line in purchase_order.order_line:
+                print(line.id)
+                print(merged_po.order_line.ids)
+                if line.product_id in [merged_line.product_id for merged_line in merged_po.order_line]: # if have same product
+                    if line.sale_order_id: # append the order line
+                        line.write({'order_id': merged_po.id})
+                    else: # add the quantity
+                        target_line = merged_po.order_line.filtered(lambda x: x.product_id == line.product_id)
+                        if target_line:
+                            target_line.product_qty = target_line.product_qty + line.product_qty
+                else: # append the order line
+                    line.write({'order_id': merged_po.id})
+
+        # else: #add the quantity
+        #     self.mapped('order_line').write({'order_id': merged_po.id})
 
         # add log note in the chatter
         message_string = ""
