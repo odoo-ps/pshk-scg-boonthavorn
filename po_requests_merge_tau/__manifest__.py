28ba52d8bd9ae534ef1f3aeb51b86d720879d26b
tau-odoo <tau@odoo.com>
2021-03-17 10:12:02 +0800

    [IMP] po_requests_merge_tau: change merge product lines method merge po for same vendor [2377007]
    [14.0.2.0]
    change merge product lines method
    If the po has source document (created from so),
    append the po lines in new po,
    else add up the quantities to the related po line in new po.

--- po_requests_merge_tau/__manifest__.py
+++ po_requests_merge_tau/__manifest__.py
@@ -1,11 +1,9 @@
 # -*- coding: utf-8 -*-
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
 'name': "po_requests_merge_tau",
-'version': '14.0.1.0',
+'version': '14.0.2.0',
 'author': 'OdooHK-TAU',
-'description': 'po request merge action',
 'depends': ['purchase'],
 'data': [
     'views/merge_po_requests.xml',
--- po_requests_merge_tau/models/merge_po_requests.py
+++ po_requests_merge_tau/models/merge_po_requests.py
@@ -1,9 +1,10 @@
+# -*- coding: utf-8 -*-
+
 from odoo import models, _
 from odoo.exceptions import UserError
 
 class PurchaseOrder(models.Model):
     _inherit = "purchase.order"
-
 
     def action_merge_po_requests(self):
         """
@@ -22,12 +23,30 @@ class PurchaseOrder(models.Model):
         if not all(vendor == purchase_order.partner_id for purchase_order in self):
             raise UserError(_('Vendor should be the same.'))
 
+        # check all are from same picking_type_id
+        picking_type_id = self[0].picking_type_id
+        if not all(picking_type_id == purchase_order.picking_type_id for purchase_order in self):
+            raise UserError(_('Operation type should be the same.'))
+
         # start merging
         merged_po = self.env['purchase.order'].create({
-            'partner_id': vendor.id
+            'partner_id': vendor.id,
+            'origin': ', '.join([rec.origin for rec in self if rec.origin ]),
+            'picking_type_id': picking_type_id.id,
         })
 
-        self.mapped('order_line').write({'order_id': merged_po.id})
+        not_to_merge_lines_ids = []
+        for purchase_order in self:
+            if not purchase_order.origin: # can merge the sale order lines
+                for line in purchase_order.order_line:
+                    target_line_to_merge = merged_po.order_line.filtered(lambda x: x.product_id == line.product_id and x.id not in not_to_merge_lines_ids)
+                    if target_line_to_merge:
+                        target_line_to_merge.product_qty += line.product_qty
+                    else:
+                        line.write({'order_id': merged_po.id})
+            else: # do not merge the sale order lines, append them
+                not_to_merge_lines_ids += purchase_order.order_line.ids
+                purchase_order.order_line.write({'order_id': merged_po.id})
 
         # add log note in the chatter
         message_string = ""
