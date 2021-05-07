911ca737c25d210b66b297667af7b0a127b250cf
nni-odoo <nni@odoo.com>
2021-05-07 13:51:09 +0800

    [ADD] product_asset_sequence

---
+++ product_asset_sequence/README.md
@@ -0,0 +1,3 @@
+# Ps-Tech module : Product Asset Sequence
+
+Add Sequencing for Product Template and Account Asset.
---
+++ product_asset_sequence/__manifest__.py
@@ -0,0 +1,28 @@
+{
+    "name": "Product Asset Sequence",  # TODO:
+    # Explain the purpose of the module
+    "summary": """
+        Product Asset Sequence scaffold module
+        """,
+    "category": "",
+    "version": "14.0.1.0.0",
+    "author": "Odoo PS",
+    "website": "http://www.odoo.com",
+    "license": "OEEL-1",
+    # TODO:
+    # Check depends order uncomment if necessary
+    "depends": [
+         'account',
+         'account_asset',
+         'product',
+    ],
+    # TODO:
+    # Check data order
+    "data": [
+        "data/sequences.xml",
+        "views/account_asset.xml",
+        "views/product_template.xml",
+    ],
+    # Only used to link to the analysis / Ps-tech store
+    "task_id": [2516119],
+}
